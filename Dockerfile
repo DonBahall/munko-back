97a32e92461f87034b2ce869093090ef853aee55
DmytroCharoian <dimachar2004@gmail.com>
2024-02-05 16:46:57 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM maven:3.9.2 AS build
+COPY src /home/app/src
+COPY pom.xml /home/app
+RUN mvn -f /home/app/pom.xml clean package
+
+FROM openjdk:18
+COPY --from=build /home/app/target/munko-back-0.0.1-SNAPSHOT.jar /usr/local/lib/store.jar
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "/usr/local/lib/store.jar"]
---
+++ src/main/java/com/example/munkoback/MunkoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.munkoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunkoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunkoBackApplication.class, args);
+    }
+
+}
