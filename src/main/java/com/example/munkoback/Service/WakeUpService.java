5355d9f0e5ba9700383f0e42e4fda69029558444
DmytroCharoian <dimachar2004@gmail.com>
2024-03-15 19:45:49 +0100

    clean up

--- src/main/java/com/example/munkoback/Service/WakeUpService.java
+++ src/main/java/com/example/munkoback/Service/WakeUpService.java
@@ -2,15 +2,9 @@ package com.example.munkoback.Service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -21,7 +15,6 @@ public class WakeUpService {
     private static final Logger logger = LoggerFactory.getLogger(WakeUpService.class);
     @Scheduled(fixedRate = 300000) // 5 min
     public void keepAlive() {
-        logger.info("Response from GraphQL server: {}");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
 
@@ -34,14 +27,12 @@ public class WakeUpService {
 
         HttpEntity<String> request = new HttpEntity<>(graphqlQuery, headers);
 
-        ResponseEntity<String> response = new RestTemplate().exchange(
+         new RestTemplate().exchange(
                 graphqlUrl,
                 HttpMethod.POST,
                 request,
                 String.class
         );
 
-        String responseBody = response.getBody();
-        logger.info("Response from GraphQL server: {}", responseBody);
     }
 }
