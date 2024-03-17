b70276b9ddea94d8224f200ae776f3be6a43f02c
DmytroCharoian <dimachar2004@gmail.com>
2024-03-17 15:52:21 +0100

    clean up and little change

--- src/main/java/com/example/munkoback/Service/WakeUpService.java
+++ src/main/java/com/example/munkoback/Service/WakeUpService.java
@@ -1,7 +1,5 @@
 package com.example.munkoback.Service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,10 +7,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+
 @Configuration
 @EnableScheduling
 public class WakeUpService {
-    private static final Logger logger = LoggerFactory.getLogger(WakeUpService.class);
+
     @Scheduled(fixedRate = 300000) // 5 min
     public void keepAlive() {
         HttpHeaders headers = new HttpHeaders();
